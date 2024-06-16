194a96617f514e44e57a58980fb65f2988e52ef0
Leo <leoniro@gmail.com>
2024-06-16 10:44:38 -0300

    auth and users modules boilerplate

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { MongoClient } from 'mongodb';
 
 @Injectable()
 export class AppService {
+  db = MongoClient.connect('localhost:27017')
   getHello(): string {
+    
     return 'Hello World!';
   }
 }
