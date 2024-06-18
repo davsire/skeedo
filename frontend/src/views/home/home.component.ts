7281002b426afc6017f2f59e849e96587db460ac
Davi Vitorino da Silva <davi.silva@softplan.com.br>
2024-06-17 23:39:06 -0300

    feat: adiciona listagem de eventos marcados

--- frontend/src/views/home/home.component.ts
+++ frontend/src/views/home/home.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PaginatorState } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  readonly paginatorFirst: number = 0;
+  readonly paginatorRows: number = 2;
+  readonly numberParticipantsShow: number = 2;
+  readonly columns: string[] = ['Nome do evento', 'Data do evento', 'Participantes'];
+
+  events: any[] = [
+    {
+      name: 'Fazer trabalho',
+      date: new Date('2024-06-01'),
+      participants: ['Davi', 'Leonardo', 'Gabriel'],
+    },
+    {
+      name: 'Sair para caminhar',
+      date: new Date('2024-06-12'),
+      participants: ['Alguém'],
+    },
+    {
+      name: 'Piquenique',
+      date: new Date('2024-06-20'),
+      participants: ['Fulano', 'Ciclano'],
+    },
+    {
+      name: 'Outro evento bacana',
+      date: new Date('2024-06-31'),
+      participants: ['ABC', 'DEF', 'GHI', 'JKL', 'MNO'],
+    },
+  ]; // @TODO: replace this mock to a call to get events endpoint
+  paginatedEvents: any [] = [];
+
+  public ngOnInit(): void {
+    this.onPageChange({first: this.paginatorFirst, rows: this.paginatorRows});
+  }
+
+  public getParticipants(participants: string[]): string {
+    const firstParticipants = participants.slice(0, this.numberParticipantsShow);
+    const lastParticipantsCount = participants.slice(this.numberParticipantsShow).length;
+    return firstParticipants.join(', ') + (lastParticipantsCount ? ` e mais ${lastParticipantsCount}` : '');
+  }
+
+  public onPageChange(event: PaginatorState): void {
+    this.paginatedEvents = this.events.slice(event.first, event.first + event.rows);
+  }
 }
--- frontend/src/views/home/home.module.ts
+++ frontend/src/views/home/home.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardModule } from 'primeng/card';
+import { TooltipModule } from 'primeng/tooltip';
+import { TableModule } from 'primeng/table';
 import { EventModalModule } from 'src/components/event-modal/event-modal.module';
 import { HomeComponent } from './home.component';
+import { PaginatorModule } from 'primeng/paginator';
 
 @NgModule({
   declarations: [
@@ -11,6 +14,9 @@ import { HomeComponent } from './home.component';
   imports: [
     CommonModule,
     CardModule,
+    TooltipModule,
+    TableModule,
+    PaginatorModule,
     EventModalModule,
   ]
 })
