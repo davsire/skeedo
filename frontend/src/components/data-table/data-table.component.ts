4a2e0e7d5543fc8af89d7c1f5ca3b86242de1c6a
Davi Vitorino da Silva <davi.silva@softplan.com.br>
2024-06-23 22:36:13 -0300

    feat: adiciona resposividade ao paginador do data-table

--- frontend/src/components/data-table/data-table.component.ts
+++ frontend/src/components/data-table/data-table.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit, TemplateRef } from '@angular/core';
+import { Component, DestroyRef, Input, OnInit, TemplateRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { BreakpointObserver, BreakpointState, Breakpoints } from '@angular/cdk/layout';
 import { PaginatorState } from 'primeng/paginator';
 
 @Component({
@@ -14,14 +16,28 @@ export class DataTableComponent implements OnInit {
 
   readonly paginatorFirst: number = 0;
   readonly paginatorRows: number = 2;
+  readonly paginatorRowsPerPageOptions: number[] = [1, 2, 5];
 
   paginatedItems: any[] = [];
+  isMobile = false;
+  destroyRef = inject(DestroyRef);
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+  }
 
   public ngOnInit(): void {
+    this.initBreakpointObserver();
     this.onPageChange({first: this.paginatorFirst, rows: this.paginatorRows});
   }
 
   public onPageChange(event: PaginatorState): void {
     this.paginatedItems = this.items.slice(event.first, event.first + event.rows);
+  }
+
+  private initBreakpointObserver(): void {
+    this.breakpointObserver
+      .observe(Breakpoints.XSmall)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((state: BreakpointState) => this.isMobile = state.matches);
   }
 }
