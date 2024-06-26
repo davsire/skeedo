b6cdcb75fd9b92c8b8b265dfef0866d95e73a89c
Davi Vitorino da Silva <davi.silva@softplan.com.br>
2024-06-25 23:48:43 -0300

    feat: adiciona url base no service de api

--- frontend/src/services/api.service.ts
+++ frontend/src/services/api.service.ts
@@ -7,26 +7,28 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
 
+  private static readonly BASE_URL = 'http://localhost:3000/';
+
   constructor(private httpClient: HttpClient) {}
 
   public get<T>(path: string): Observable<T> {
     const options = this.getRequestOptions();
-    return this.httpClient.get<T>(path, options);
+    return this.httpClient.get<T>(`${ApiService.BASE_URL}${path}`, options);
   }
 
   public post<T>(path: string, data: any): Observable<T> {
     const options = this.getRequestOptions();
-    return this.httpClient.post<T>(path, JSON.stringify(data), options);
+    return this.httpClient.post<T>(`${ApiService.BASE_URL}${path}`, JSON.stringify(data), options);
   }
 
   public put<T>(path: string, data: any): Observable<T> {
     const options = this.getRequestOptions();
-    return this.httpClient.put<T>(path, JSON.stringify(data), options);
+    return this.httpClient.put<T>(`${ApiService.BASE_URL}${path}`, JSON.stringify(data), options);
   }
 
   public delete<T>(path: string): Observable<T> {
     const options = this.getRequestOptions();
-    return this.httpClient.delete<T>(path, options);
+    return this.httpClient.delete<T>(`${ApiService.BASE_URL}${path}`, options);
   }
 
   private getRequestOptions(): {headers: HttpHeaders} {
