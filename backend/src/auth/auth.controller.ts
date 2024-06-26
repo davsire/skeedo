7112835cca2294b88e86efd567ef3c3820c2820a
Leo <leoniro@gmail.com>
2024-06-25 22:19:59 -0300

    user create meio feito

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserLogInDto } from 'src/domain/dto/userLogInDto';
+import { CreateUserDto } from 'src/domain/dto/createUserDto';
 
 @Controller('auth')
 export class AuthController {
@@ -11,6 +12,14 @@ export class AuthController {
     const username = userLogInDto.username;
     const password = userLogInDto.password;
     return this.authService.login(username, password);
+  }
+
+  @Get('/register')
+  create(createUserDto: CreateUserDto): string {
+    const username = createUserDto.username;
+    const password = createUserDto.password;
+    const displayName = createUserDto.displayName
+    return this.authService.create(username, password, displayName);
   }
 
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from 'schemas/user.schema';
+import { CreateUserDto } from 'src/domain/dto/createUserDto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
+  constructor(@InjectModel(User.name) private readonly model: Model<UserDocument>) {}
   login(username: string, password: string): string {  
-    return 'Hello World!';
+    // this.model.find()
+    return 'yo';
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    // check if user already exists
+    const exists = await this.model.find({ username: createUserDto.username }).exec();
+
+    if (exists) {
+      throw("Usuário com esse nome já existe")
+    }
+
+    // generate random salt
+    const salt = 10;
+    const hash = await bcrypt.hash(createUserDto.password, salt);
+
+    // create new user
+    return await new this.model({
+      ...createUserDto,
+      passwordHash: hash,
+      salt: salt,
+      createdAt: new Date(),
+    }).save();
   }
 }
