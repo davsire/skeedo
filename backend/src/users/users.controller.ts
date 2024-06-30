53384418f909f4cf73f2644f7f3f48b7d37e180a
Leo <leoniro@gmail.com>
2024-06-30 03:51:19 -0300

    user crud working

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Request, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { UpdateUserDto } from './dto/updateUser.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @UseGuards(AuthGuard)
+  @Get()
+  async findAll() {
+    return this.userService.findAll();
+  }
+
+  @UseGuards(AuthGuard)
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
+  
+  @UseGuards(AuthGuard)
+  @Patch(':id')
+  modify(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto, @Request() req) {
+    return this.userService.update(id, updateUserDto, req.user);
+  }
+
+  @UseGuards(AuthGuard)
+  @Delete(':id')
+  remove(@Param('id') id: string, @Request() req) {
+    return this.userService.remove(id, req.user);
+  }
+
+}
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'schemas/user.schema';
@@ -8,16 +8,49 @@ import { UserSignUpDto } from 'src/users/dto/userSignUp.dto';
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  async create(createUserDto: UserSignUpDto): Promise<User> {
-    const newUser = new this.userModel(createUserDto);
-    return newUser.save();
+  async create(createUserDto: UserSignUpDto) {
+    // user creation is handled by auth module
   }
 
   async findAll(): Promise<User[]> {
-    return this.userModel.find().exec();
+    return this.userModel.find();
   }
 
-  async findOne(username: string): Promise<User | undefined> {
-    return undefined;   
+  async findOne(id) {
+    try {
+      const user = await this.userModel.findById(id);
+      return user;
+    }
+    catch {
+      throw new NotFoundException();
+    }
+  }
+
+  async update(id, updateUserDto, token) {
+    try {
+      if (id !== token.sub) {
+        throw new UnauthorizedException();
+      }
+      
+      return await this.userModel.findByIdAndUpdate(
+        id,
+        {displayName: updateUserDto.displayName},
+        {returnDocument: "after"}
+      );
+    } catch {
+      throw new NotFoundException();
+    }
+  }
+
+  async remove(id, token) {
+    try {
+      if (id !== token.sub) {
+        throw new UnauthorizedException();
+      }
+      
+      return await this.userModel.findByIdAndDelete(id);
+    } catch {
+      throw new NotFoundException();
+    }
   }
 }
