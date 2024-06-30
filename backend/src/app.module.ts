8d4d6ee4f4a9ed954a3229472b5e22c8091e14d1
Leo <leoniro@gmail.com>
2024-06-30 10:05:07 -0300

    update README

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,16 +4,14 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
 import { EventsModule } from './events/events.module';
 import { InvitesModule } from './invites/invites.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/27017'),
+    MongooseModule.forRoot('mongodb://localhost/app'),
     AuthModule,
     UsersModule,
-    ConfigModule.forRoot(),
     EventsModule,
     InvitesModule
   ],
