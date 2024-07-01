cbcce144b82586a76cdce589ab219b55c2e5d506
Davi Vitorino da Silva <davi.silva@softplan.com.br>
2024-06-30 23:41:13 -0300

    fix: ajuste bobo de validação de form

--- frontend/src/components/user-data/user-data.component.ts
+++ frontend/src/components/user-data/user-data.component.ts
@@ -40,10 +40,10 @@ export class UserDataComponent implements OnInit {
   private initUserData(): void {
     if (this.userData) return;
     this.userData = this.formBuilder.group({
-      [this.fieldName]: [{value: null, disabled: this._disableFields}, Validators.required],
-      [this.fieldUsername]: [{value: null, disabled: this._disableFields}, Validators.required],
-      [this.fieldPassword]: [{value: null, disabled: this._disableFields}, [this.passwordRequiredValidator.bind(this), this.passwordValidator.bind(this)]],
-      [this.fieldConfirmPassword]: [{value: null, disabled: this._disableFields}, [this.passwordRequiredValidator.bind(this), this.confirmPasswordValidator.bind(this)]],
+      [this.fieldName]: [{value: '', disabled: this._disableFields}, Validators.required],
+      [this.fieldUsername]: [{value: '', disabled: this._disableFields}, Validators.required],
+      [this.fieldPassword]: [{value: '', disabled: this._disableFields}, [this.passwordRequiredValidator.bind(this), this.passwordValidator.bind(this)]],
+      [this.fieldConfirmPassword]: [{value: '', disabled: this._disableFields}, [this.passwordRequiredValidator.bind(this), this.confirmPasswordValidator.bind(this)]],
     });
     this.userDataChange.emit(this.userData);
   }
