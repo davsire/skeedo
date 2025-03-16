dfe61318018c851525cee254cddead023b9a5fd7
Davi Vitorino da Silva <davi.silva@softplan.com.br>
2025-03-16 16:44:02 -0300

    feat: dockeriza aplicacoes

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { MongoClient } from 'mongodb';
 
 @Injectable()
 export class AppService {
   getHello(res): string {
-    console.log(res)
     return 'Hello World!';
   }
 }
